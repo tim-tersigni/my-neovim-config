1977c75a720b89c149a451647b233fff10179e7e
Tim <tim.tersigni@gmail.com>
2022-12-17 17:18:02 -0600

    Edit telescope config

--- lua/tim/configs/telescope.lua
+++ lua/tim/configs/telescope.lua
@@ -1,5 +1,5 @@
 -- telescope.lua
-
+print('hello')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
